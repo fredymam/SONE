c6100f4ece8b4a027da958d7c79f6357624db1a0
alumno <alumno@LAB2-12>
2017-08-08 18:13:39 -0300

    clase alumno

---
+++ SONE/src/logica/Alumno.java
@@ -0,0 +1,12 @@
+package logica;
+
+public class Alumno {
+	public char Nombre;	
+	public char Apellido;
+	public int Curso;
+	public String Division;
+	public String Turno;
+	public int Dni;
+	
+	
+}
