c8797f34f46b3fac46241f7ec1d53f67f7b865bb
Luciano Bessoni <lucianobessoni.04@gmail.com>
2017-08-08 18:37:38 -0300

    Creación de persona

---
+++ SONE/src/logica/Persona.java
@@ -0,0 +1,14 @@
+package logica;
+
+public class Persona {
+
+	private String apellido;
+	private String nombre;
+	private int DNI;
+	private String email;
+	private int telefono;
+	public Persona() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
