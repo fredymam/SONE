5fc285efbdc1bd3d44837f86b2fbb29f8e24b554
alumno <alumno@LAB2-11>
2017-08-08 18:36:28 -0300

    getter and setter

--- SONE/src/logica/Tutor.java
+++ SONE/src/logica/Tutor.java
@@ -5,7 +5,38 @@ public class Tutor {
 	protected char Apellido;
 	protected String Email;
 	protected int Telefono;
+	public char getNombre() {
+		return Nombre;
+	}
+	public void setNombre(char nombre) {
+		Nombre = nombre;
+	}
+	public char getApellido() {
+		return Apellido;
+	}
+	public void setApellido(char apellido) {
+		Apellido = apellido;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public int getTelefono() {
+		return Telefono;
+	}
+	public void setTelefono(int telefono) {
+		Telefono = telefono;
+	}
+	public int getDni() {
+		return Dni;
+	}
+	public void setDni(int dni) {
+		Dni = dni;
+	}
 	protected int Dni;
 	
-	
+
 }
+
