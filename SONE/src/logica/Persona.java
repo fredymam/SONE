cbe3d84ec0b42ce4e13c9696c361f444445baf58
Federico Pereyra <david99pereyra@gmail.com>
2017-08-08 19:24:53 -0300

    Agregue get and set a los atributos de la clase persona

---
+++ SONE/src/logica/Persona.java
@@ -0,0 +1,54 @@
+package logica;
+
+public abstract class Persona {
+	private String nombre;
+	private String apellido;
+	private int DNI;
+	private String email;
+	private int tel;
+	
+	public Persona() {
+				
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public int getDNI() {
+		return DNI;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public int getTel() {
+		return tel;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public void setDNI(int dNI) {
+		DNI = dNI;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setTel(int tel) {
+		this.tel = tel;
+	}
+
+}
