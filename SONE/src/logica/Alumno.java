bc66feae27b635297d1eeaa7ffd4c184a16a6fbf
LuciaBaschiasueli <luciabaschiasueli@gmail.com>
2017-08-08 18:48:43 -0300

    Clase Alumno

---
+++ SONE/src/logica/Alumno.java
@@ -0,0 +1,14 @@
+package logica;
+
+public class Alumno {
+	
+	private int Curso;
+	private int División;
+	private String Turno;
+	private int DNI_Tutor;
+	
+	public Alumno() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
