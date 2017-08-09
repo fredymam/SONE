313d784c61436724127135cc066c47fa5d503e54
alumno <alumno@LAB2-07>
2017-08-09 19:40:45 -0300

    personas

---
+++ SONE/src/logica/Personas.java
@@ -0,0 +1,33 @@
+package logica;
+
+public class Personas {
+
+	protected char Nombre;	
+	protected char Apellido;
+		
+	
+	public Personas() {
+		
+	}
+
+
+	public char getNombre() {
+		return Nombre;
+	}
+
+
+	public void setNombre(char nombre) {
+		Nombre = nombre;
+	}
+
+
+	public char getApellido() {
+		return Apellido;
+	}
+
+
+	public void setApellido(char apellido) {
+		Apellido = apellido;
+	}
+
+}
