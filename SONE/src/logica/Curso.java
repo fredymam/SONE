39c27fc9b2c073429fe5bf709aa50ac55f0f357f
alumno <alumno@LAB2-03>
2017-08-16 20:23:14 -0300

    Curso amilcar

--- SONE/src/logica/Curso.java
+++ SONE/src/logica/Curso.java
@@ -2,9 +2,17 @@ package logica;
 
 public class Curso {
 	
-	protected int numero_curso;
-	protected int division;
-	protected String turno;
+	public int numero_curso;
+	public int division;
+	public String turno;
+	
+	public Curso(int numero_curso,int division, String turno) {
+		this.numero_curso = numero_curso;
+		this.turno = turno;
+		this.division = division;
+		
+	}
+	
 	
 	public int getNumero_curso() {
 		return numero_curso;
@@ -18,11 +26,14 @@ public class Curso {
 	public void setDivision(int division) {
 		this.division = division;
 	}
-	public String getTurno() {
-		return turno;
-	}
 	public void setTurno(String turno) {
 		this.turno = turno;
 	}
+	public String getTurno() {
+		return turno;
+	}
+	
+	
+	
 	
 }
