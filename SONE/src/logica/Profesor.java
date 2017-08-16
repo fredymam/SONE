e4c981153389d545176590c8d6083177e425a465
alumno <alumno@LAB2-09>
2017-08-16 19:38:34 -0300

    profesor

---
+++ SONE/src/logica/Profesor.java
@@ -0,0 +1,11 @@
+package logica;
+
+public class Profesor {
+	protected char Nombre;
+	protected char Apellido;
+	protected String Materia;
+	protected String escuela;
+	protected int Numero;
+	
+
+}
