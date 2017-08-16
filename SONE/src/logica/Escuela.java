fb1971345193ea3c5026f192aa71fec231101605
Federico Pereyra <david99pereyra@gmail.com>
2017-08-16 20:26:26 -0300

    Agregue la clase Escuela

---
+++ SONE/src/logica/Escuela.java
@@ -0,0 +1,16 @@
+package logica;
+
+public class Escuela {
+	private String nombre;
+	private int cue;
+	private int tel;
+	private String correo;
+	
+	public Escuela(){
+		
+	}
+	
+	public void RegistroCelador(){
+		
+	}
+}
