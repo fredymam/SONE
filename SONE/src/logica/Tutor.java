252f1974a1f888df9d9de4450741269b693164ea
poncemicaela <micaelaponce1999@gmail.com>
2017-08-16 20:03:40 -0300

    se esta creando el codigo de la clase tutor

--- SONE/src/logica/Tutor.java
+++ SONE/src/logica/Tutor.java
@@ -1,5 +1,28 @@
 package logica;
 
 public class Tutor {
+	public String Nombre;
+	public String Apellido; 
+	public String Correo;
+	public int DNI; 
+	public int Tel;  
 	
+	public Tutor (String Nombre, String Apellido, String Correo, int DNI, int Tel){
+		
+	}
+	public String getNombre(){
+		return Nombre;
+	}
+	public String getApellido(){
+		return Apellido;
+	}
+	public String getCorreo(){
+		return Correo;
+	}
+	public int getDNI(){
+		return DNI;
+	}
+	public int getTel(){
+		return Tel;
+	}
 }
