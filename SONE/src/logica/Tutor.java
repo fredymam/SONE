75f099c79f68e677432b3803a8a52d7299da1195
alumno <alumno@LAB2-18>
2017-08-22 18:57:49 -0300

    .....

--- SONE/src/logica/Tutor.java
+++ SONE/src/logica/Tutor.java
@@ -5,42 +5,44 @@ public class Tutor extends Persona {
 	private int DNI_Alumno;
 	private String email;
 	private String N_Celular;
+	private boolean Notificar_email;
+	private boolean Notificar_SMS;
+	
 	
 	public Tutor(String nombre, String apellido, int dni) {
 		super(nombre, apellido, dni);
-		
-	}
+		}
 	
-	public int getDNI_Alumno() {
-		return DNI_Alumno;
-	}
+		public int getDNI_Alumno() {
+			return DNI_Alumno;
+		}
 
 
-	public void setDNI_Alumno(int dNI_Alumno) {
-		DNI_Alumno = dNI_Alumno;
-	}
+		public void setDNI_Alumno(int dNI_Alumno) {
+			DNI_Alumno = dNI_Alumno;
+		}
 
 
-	public String getEmail() {
-		return email;
-	}
+		public String getEmail() {
+			return email;
+		}
 
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+		public void setEmail(String email) {
+			this.email = email;
+		}
 
 
-	public String getN_Celular() {
+		public String getN_Celular() {
 		return N_Celular;
-	}
+		}	
 
 
-	public void setN_Celular(String n_Celular) {
-		N_Celular = n_Celular;
-	}
+		public void setN_Celular(String n_Celular) {
+			N_Celular = n_Celular;
+		}
 
-
+		
 	
 		
 }
