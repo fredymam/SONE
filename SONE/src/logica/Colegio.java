104c130cf71b1cc11f2c9b07c0d634533b1eb19a
Wences9 <wencemaza5@gmail.com>
2017-08-22 17:29:06 -0300

    Colegio

---
+++ SONE/src/logica/Colegio.java
@@ -0,0 +1,16 @@
+package logica;
+
+public class Colegio {
+	private String nombre;
+	private int cue;
+	private int tel;
+	private String correo;
+	
+	public Colegio(){
+		
+	}
+	
+	public void RegistroCelador(){
+		
+	}
+}
