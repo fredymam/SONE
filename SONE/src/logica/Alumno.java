11e0e0001c18e7fa9fb3f600fd09ec6348cabf2e
alumno <alumno@LAB2-03>
2017-08-22 17:54:00 -0300

    c

--- SONE/src/logica/Alumno.java
+++ SONE/src/logica/Alumno.java
@@ -14,12 +14,3 @@ public class Alumno extends Persona {
 	
 }
 
-/*1.Definir	ArayListcclases> nombre >>ArrayList<Alumno>alumnos;
-2.Crear Alumnos = new ArrayList<alumno>;
-3.Metodos -get alumnos.get(3);
-		  -add alumnos.add(a1);//alumno.add(new Alumno('Nahuel');
-		  -size alumnos.size();
-		  -set alumnos.set(2,a2);*/
-/* 
- public
- */
