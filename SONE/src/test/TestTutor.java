1e468f630d7f40341d2552fc23a9e4aad92d00f2
Amores <alumno@LAB2-02>
2017-08-22 17:20:19 -0300

    2

--- SONE/src/test/TestTutor.java
+++ SONE/src/test/TestTutor.java
@@ -1,9 +1,48 @@
 package test;
+import logica.Tutor;
+import logica.Alumno;
 
 public class TestTutor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Tutor t1;
+		t1 = new Tutor("Tutor", "Garca1", 456456, "correogmail" ,230254684);
+		System.out.println(t1.getNombre());
+		System.out.println(t1.getApellido());
+		System.out.println(t1.getDni());
+		System.out.println(t1.getCorreo());
+		System.out.println(t1.getTelefono());
+		System.out.println(t1.getAlumnos());
+		System.out.println((""));
+		System.out.println((""));
+		
+		Tutor t2;
+		t2 = new Tutor("Tutor", "Garca2", 6786786, "correogmail" ,230254684);
+		System.out.println(t2.getNombre());
+		System.out.println(t2.getApellido());
+		System.out.println(t2.getDni());
+		System.out.println(t1.getCorreo());
+		System.out.println(t1.getTelefono());
+				System.out.println(t2.getAlumnos());
+		System.out.println((""));
+		System.out.println((""));
+		
+		Tutor t3;
+		t3 = new Tutor("Tutor", "Garca3", 2132341, "correogmail" ,230254684);
+		System.out.println(t3.getNombre());
+		System.out.println(t3.getApellido());
+		System.out.println(t3.getDni());
+		System.out.println(t1.getCorreo());
+		System.out.println(t1.getTelefono());
+		System.out.println(t3.getAlumnos());
+		
+		//System.out.println(t3.getFaltas());
+		//System.out.println(t3.getConsultarasistencias());
+		
+		System.out.println((""));
+		System.out.println((""));
 
 	}
 
