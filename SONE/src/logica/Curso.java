2f0801f85b32362cb0c5954774d5da5acb497c2d
alumno <alumno@LAB2-11>
2017-08-22 17:33:04 -0300

    arraylist

--- SONE/src/logica/Curso.java
+++ SONE/src/logica/Curso.java
@@ -1,12 +1,14 @@
 package logica;
 
+import java.util.ArrayList;
+
 public class Curso {
 	public int Curso;
 	public int Division;
 	public char Turno;
-	ArrayList<Alumno>ListaAlumnos;
+	public ArrayList<Alumno> ListaAlumnos;
 	public Curso() {
-		
+		ListaAlumnos = new ArrayList<Alumno>();
 	}
 	
 	
