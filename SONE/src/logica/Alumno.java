754a906e52dcc95cf3cac71c8cf341fffce8ca4f
Koa1 <nico.997@hotmail.com>
2017-08-23 20:19:42 -0300

    Error en base a ajuste (Re-plantenado)

--- SONE/src/logica/Alumno.java
+++ SONE/src/logica/Alumno.java
@@ -1,15 +1,8 @@
 package logica;
 
-public class Alumno extends Persona {
- 
-	public Tutor tutor;
-	public Curso curso;
-	
-	public Alumno(String nombre, String apellido, int dni, Curso curso) {
-		super(nombre, apellido, dni);
-		this.curso = curso;
+public class Alumno {
+
 	}
 
 
 	
-}
