4ec8d25a1b599a089658716c01ad2b230185a98d
Koa1 <nico.997@hotmail.com>
2017-08-23 20:52:39 -0300

    Creacion notas

---
+++ SONE/src/Notas/Notas.java
@@ -0,0 +1,19 @@
+
+/**
+ * Escuela escuela =New Escuela(1,"Epet3"); # cue,nombre
+ * Curso c1 = new Curso(4,1);  #año division
+ * Escuela.agregarcurso(C1);
+ * C1.agregarAlumno(new Alumno(62626263,Marcelo,Mana); #DNI ,Nombre,Apellido
+ * C1.agregarAlumno(new Alumno(63636362,Magnus,Magniten); #DNI ,Nombre,Apellido
+ * ETC...
+ * C1.notificar(10:43,"Falta profesor programacion");#Hora,Motivo
+ * ---------------------------------------------------------------------------------------
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *
+ */
+
