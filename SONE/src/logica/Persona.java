932b37b134ab5cfc2e39065a8324311bcf8de5a3
Amores <alumno@LAB2-02>
2017-08-23 20:51:37 -0300

    23/08

--- SONE/src/logica/Persona.java
+++ SONE/src/logica/Persona.java
@@ -1,7 +1,6 @@
 package logica;
-import java.util.*;
 
-public abstract class Persona implements Comparable {
+public abstract class Persona {
 	
 	protected String nombre;
 	protected String apellido;
@@ -47,4 +46,105 @@ public abstract class Persona implements Comparable {
 		this.telefono = telefono;
 	}
 	
+
+
+
+
+
+
+	
 }
+
+
+
+
+
+
+
+/*
+import java.util.*;
+class Alumnos{
+   private int nota;
+   private String nombre;
+   public void setNota(int nota){
+      this.nota = nota;
+   }
+   public int getNota( ){
+      return this.nota;
+   }
+   public void setNombre(String nombre){
+      this.nombre = nombre;
+   }
+   public String getNombre( ){
+      return this.nombre;
+   }
+}
+class ComparaNota implements Comparator{/**hay que implementar la interfazComparator y definir el método compare()
+   public int compare (Object alum1, Object alum2){
+      int alum1Nota = ((Alumnos)alum1).getNota( );//hacemos un casting
+      int alum2Nota = ((Alumnos)alum2).getNota( );
+      if(alum1Nota > alum2Nota){//ordenamos de mayor a menor
+         return -1;
+      }else if(alum1Nota < alum2Nota){
+         return 1;
+      }else{
+         return 0;
+      }
+   }
+}
+class ComparaNombre implements Comparator{
+   public int compare(Object alum1, Object alum2){
+      String alum1Nombre = ((Alumnos) alum1).getNombre( );
+      String alum2Nombre = ((Alumnos) alum2).getNombre( );
+      return alum1Nombre.compareTo(alum2Nombre);/**usamos el método compareTo para un orden  alfabético       
+   }
+}
+public class EjemploComparatorJava{//creamos el array y le añadimos valores
+   public static void main(String[ ] args){
+      Alumnos alumnos[ ] = new Alumnos[3];
+      alumnos[0] = new Alumnos( );
+      alumnos[0].setNota(8);
+      alumnos[0].setNombre("Ana");
+      alumnos[1] = new Alumnos( );
+      alumnos[1].setNota(6);
+      alumnos[1].setNombre("Xose");
+      alumnos[2] = new Alumnos( );
+      alumnos[2].setNota(3);
+      alumnos[2].setNombre("Carlos");
+      System.out.println("Alumnos desordenados");
+      for(int i = 0; i<alumnos.length;i++){
+         System.out.println("Alumno "+(i+1)+" Nombre: "+alumnos[i].getNombre( )+" Nota: "+alumnos[i].getNota( ));
+      }
+      Arrays.sort(alumnos, new ComparaNota( ));/**usamos el método sort para ordenar las notas
+      System.out.println("Alumnos ordenados por notas");
+      for(int i = 0; i<alumnos.length; i++){
+         System.out.println("Alumno "+(i+1)+" Nombre: "+alumnos[i].getNombre( )+" Nota: "+alumnos[i].getNota( ));
+      }
+      Arrays.sort(alumnos, new ComparaNombre( ));/**ahora ordenamos los nombres
+      System.out.println("Alumnos ordenados por nombre");
+      for(int i = 0;i<alumnos.length; i++){
+         System.out.println("Alumno "+(i+1)+" Nombre:     "+alumnos[i].getNombre( )+" Nota: "+alumnos[i].getNota( ));
+      }
+   }
+}
+
+
+
+   La salida será como sigue:
+
+
+Alumnos desordenados
+Alumno 1 Nombre: Ana Nota: 8
+Alumno 2 Nombre: Xose Nota: 6
+Alumno 3 Nombre: Carlos Nota: 3
+Alumnos ordenados por notas
+Alumno 1 Nombre: Ana Nota: 8
+Alumno 2 Nombre: Xose Nota: 6
+Alumno 3 Nombre: Carlos Nota: 3
+Alumnos ordenados por nombres
+Alumno 1 Nombre: Ana Nota: 8
+Alumno 2 Nombre: Carlos Nota: 3
+Alumno 3 Nombre: Xose Nota: 6
+*/
+
+
