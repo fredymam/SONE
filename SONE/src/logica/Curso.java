cc9b934d3eae9916cd18376367d1e9129ff71800
Koa1 <nico.997@hotmail.com>
2017-08-29 19:34:17 -0300

    Modificacion Curso

--- SONE/src/logica/Curso.java
+++ SONE/src/logica/Curso.java
@@ -1,8 +1,79 @@
 package logica;
+import java.util.ArrayList;
 
 public class Curso {
-	public int Curso;
-	public int Division;
-	public String Turno;
+	private ArrayList<Alumno> alumnos;
+	private int curso; // numero entero
+	private int division; // numero entero
+	private char turno; // basado en variables T tarde M mañana V vespertino
+	private int celador; // en base a id o numero
+	
+	
+	
+	public Curso(int c, int d, char t) {
+		alumnos = new ArrayList<Alumno>();
+		curso = c;
+		division = d;
+		turno = t;
+	}
+	
+	public ArrayList<Alumno> getAlumnos() {
+		return alumnos;
+	}
+
+	public void setAlumnos(ArrayList<Alumno> alumnos) {
+		this.alumnos = alumnos;
+	}
+
+	public int getCurso() {
+		return curso;
+	}
+
+	public void setCurso(int curso) {
+		this.curso = curso;
+	}
+
+	public int getDivision() {
+		return division;
+	}
+
+	public void setDivision(int division) {
+		this.division = division;
+	}
+
+	public int getCelador() {
+		return celador;
+	}
+
+	public void setCelador(int celador) {
+		this.celador = celador;
+	}
+
+	public char getTurno() {
+		return turno;
+	}
+
+	public void setTurno(char turno) {
+		this.turno = turno;
+	}
+
+	void AsignarCelador(Celador cel){
+		celador = cel.getNum_celador();
+	}
+	
+	boolean Notificar(){
+		return true;
+	}
+	
+	void AgregarAlumno(Alumno a){
+		alumnos.add(a);
+	}
+	
+	boolean Quitar(Alumno a){
+		alumnos.remove(a);
+		return true;
+	}
+	
+	
 	
 }
